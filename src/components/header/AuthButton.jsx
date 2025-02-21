38632316341c8bcbcb17fbcb7d4fadd610ef9527
choichangyeon <changyon99@naver.com>
2025-02-21 16:59:43 +0900

    design:#5 Auth 버튼 라우터 설정

--- src/components/header/AuthButton.jsx
+++ src/components/header/AuthButton.jsx
@@ -1,16 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 const AuthButton = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-row justify-center align-center gap-5">
       <button
         type="button"
         className="rounded-md px-2 py-1 transition duration-300 hover:text-white hover:bg-indigo-300"
+        onClick={() => navigate("/login")}
       >
         로그인
       </button>
       <button
         type="button"
         className="rounded-md px-2 py-1 transition duration-300 hover:text-white hover:bg-indigo-300"
-        onClick={() => {}}
+        onClick={() => navigate("/signup")}
       >
         회원가입
       </button>
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,12 +1,14 @@
 import AuthButton from "@components/header/AuthButton";
 import MypageButton from "@components/header/MypageButton";
+import { useNavigate } from "react-router-dom";
 
 const isAuth = false;
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className="w-full h-md flex flex-row justify-between items-center px-10">
-      <button>홈</button>
+      <button onClick={() => navigate("/")}>홈</button>
       {isAuth ? <MypageButton /> : <AuthButton />}
     </div>
   );
