7bc018d5c07ac87939f0d77fc98cf9fc538412f8
choichangyeon <changyon99@naver.com>
2025-02-24 00:30:39 +0900

    design:#5 포커스 색상 변경

--- src/components/common/InputForm.jsx
+++ src/components/common/InputForm.jsx
@@ -1,5 +1,10 @@
 const InputForm = ({ ...props }) => {
-  return <input className="h-sm border rounded-lg my-2 px-2" {...props} />;
+  return (
+    <input
+      className="h-sm border focus:outline-none focus:border-indigo-300 rounded-lg my-2 px-2"
+      {...props}
+    />
+  );
 };
 
 export default InputForm;
