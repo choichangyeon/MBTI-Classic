5155e208c098cf5054064ceeaa4761003293044b
choichangyeon <changyon99@naver.com>
2025-02-24 08:42:45 +0900

    feat:#5 마이페이지 화면 구성

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { useState } from "react";
+import InputForm from "@components/common/InputForm";
+
+const MOK_USER = {
+  id: "최창연",
+  nickname: "체리창연",
+};
 
 const Mypage = () => {
-  return <div>Mypage</div>;
+  const [id, setId] = useState(MOK_USER.id);
+  const [nickname, setNickname] = useState(MOK_USER.nickname);
+
+  const handleChangeUserNickname = () => {
+    console.log({ id, nickname });
+
+    //닉네임 변경 로직
+  };
+  return (
+    <div className="col-base center-base my-40">
+      <div className="col-base center-base h-96 w-80 border ">
+        <InputForm
+          type="text"
+          placeholder="아이디"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <InputForm
+          type="text"
+          placeholder="닉네임"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+        />
+        <div className="col-base center-base my-2">
+          <button
+            className="btn-base my-2 w-md h-xs"
+            onClick={handleChangeUserNickname}
+          >
+            정보수정
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Mypage;
