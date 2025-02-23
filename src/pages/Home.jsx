9c7e957dd38a106f61c684cf3902b5f579fe7850
choichangyeon <changyon99@naver.com>
2025-02-23 23:18:34 +0900

    feat:#5 테스트 화면 이동 버튼 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,32 @@
 import { POST_CONTENT, POST_TITLE } from "@/data/HomePostCardContent";
 import PostCard from "@components/home/PostCard";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
-    <div className="flex flex-row justify-evenly my-40">
-      <PostCard title={POST_TITLE.TEST_TITLE}>
-        {POST_CONTENT.TITLE_CONTENT}
-      </PostCard>
-      <PostCard title={POST_TITLE.TEST_EFFECT_1}>
-        {POST_CONTENT.EFFECT_CONTENT_1}
-      </PostCard>
-      <PostCard title={POST_TITLE.TEST_EFFECT_2}>
-        {POST_CONTENT.EFFECT_CONTENT_2}
-      </PostCard>
+    <div className="flex flex-col justify-center my-40">
+      <div className="flex flex-row justify-evenly">
+        <PostCard title={POST_TITLE.TEST_TITLE}>
+          {POST_CONTENT.TITLE_CONTENT}
+        </PostCard>
+        <PostCard title={POST_TITLE.TEST_EFFECT_1}>
+          {POST_CONTENT.EFFECT_CONTENT_1}
+        </PostCard>
+        <PostCard title={POST_TITLE.TEST_EFFECT_2}>
+          {POST_CONTENT.EFFECT_CONTENT_2}
+        </PostCard>
+      </div>
+      <div className="flex my-20 justify-center">
+        <button
+          onClick={() => {
+            navigate("/test");
+          }}
+          className="h-sm w-lg transition duration-300 shadow-lg hover:text-white hover:border-indigo-300 hover:bg-indigo-300 border-2 rounded-md"
+        >
+          테스트하러 가기
+        </button>
+      </div>
     </div>
   );
 };
