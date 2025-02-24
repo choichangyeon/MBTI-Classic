1e34b76b84dad195b215a2d0ac8863aff5176230
choichangyeon <changyon99@naver.com>
2025-02-24 15:42:12 +0900

    feat:#11 로그아웃 구현

--- src/components/header/MypageButton.jsx
+++ src/components/header/MypageButton.jsx
@@ -1,15 +1,29 @@
+import useUserStore from "@/app/userStore";
 import { useNavigate } from "react-router-dom";
 
 const MypageButton = () => {
   const navigate = useNavigate();
+  const clearUserData = useUserStore((state) => state.clearUserData);
   return (
-    <button
-      type="button"
-      className="btn-base px-2 py-1"
-      onClick={() => navigate("/mypage")}
-    >
-      마이페이지
-    </button>
+    <div className="row-base center-base gap-5">
+      <button
+        type="button"
+        className="btn-base px-2 py-1"
+        onClick={() => navigate("/mypage")}
+      >
+        마이페이지
+      </button>
+      <button
+        type="button"
+        className="btn-base px-2 py-1"
+        onClick={() => {
+          clearUserData();
+          navigate("/");
+        }}
+      >
+        로그아웃
+      </button>
+    </div>
   );
 };
 
