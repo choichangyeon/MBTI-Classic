cfb50ab331e9cd3deb655ffeeb3f9df789cf42ea
choichangyeon <changyon99@naver.com>
2025-02-24 11:39:15 +0900

    feat:#11 zustand 로직 업데이트

--- src/app/bearStore.js
+++ src/app/bearStore.js
@@ -1,14 +1,42 @@
 import { create } from "zustand";
 
+const initialUserData = {
+  id: null,
+  nickname: "",
+};
+
 const useBearsStore = create((set) => ({
-  userData: {},
+  userData: { ...initialUserData },
+
+  // 사용자 데이터를 설정하고 localStorage에 저장
   setUserData: (id, nickname) => {
-    set(() => ({ userData: { id, nickname } }));
-    localStorage.setItem(id, nickname);
+    const newUserData = { id, nickname };
+    set(() => ({ userData: newUserData }));
+
+    // 브라우저 환경 체크
+    if (typeof window !== "undefined") {
+      localStorage.setItem("userData", JSON.stringify(newUserData));
+    }
   },
+
+  // 사용자 닉네임만 업데이트
   updateUserNickname: (nickname) =>
-    set((state) => ({ userData: { ...state.userData, nickname } })),
-  clearUserData: () => set(() => ({ userData: {} })),
+    set((state) => {
+      const updatedUserData = { ...state.userData, nickname };
+
+      if (typeof window !== "undefined") {
+        localStorage.setItem("userData", JSON.stringify(updatedUserData));
+      }
+      return { userData: updatedUserData };
+    }),
+
+  // 사용자 데이터를 초기 상태로 리셋
+  clearUserData: () => {
+    set(() => ({ userData: { ...initialUserData } }));
+    if (typeof window !== "undefined") {
+      localStorage.removeItem("userData");
+    }
+  },
 }));
 
 export default useBearsStore;
