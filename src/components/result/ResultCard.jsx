8d6bbb3e8a3de036f3dc66702ef489854aa005ae
choichangyeon <changyon99@naver.com>
2025-02-24 20:01:45 +0900

    feat:#15 테스트 결과 페이지 작동 구현

--- src/components/result/ResultCard.jsx
+++ src/components/result/ResultCard.jsx
@@ -3,7 +3,7 @@ import { MBTI_Descriptions } from "@/data/MBTIDescriptions";
 const ResultCard = ({ content }) => {
   const { id, userId, result, visibility } = content;
   return (
-    <div className="col-base flex-wrap  w-xl h-xl cursor-pointer shadow-lg hover:-translate-y-4 transition duration-300 border rounded-md px-4 bg-amber-50">
+    <div className="col-base w-3/4 h-xl cursor-pointer shadow-lg hover:-translate-y-4 transition duration-300 border rounded-md px-4 bg-amber-50">
       <h1>{result}</h1>
       <p>{MBTI_Descriptions[result]}</p>
     </div>
