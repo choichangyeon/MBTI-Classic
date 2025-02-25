ef8dc0f9a1f1c3bce1caf2a287171bbf9c57b6ca
choichangyeon <changyon99@naver.com>
2025-02-25 09:22:50 +0900

    feat:#18 프로필 변경 alert 변경

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import InputForm from "@components/common/InputForm";
 import useUserStore from "@/app/userStore";
 import { updateProfile } from "@/api/auth";
+import Swal from "sweetalert2";
 
 const Mypage = () => {
   const userData = useUserStore((state) => state.userData);
@@ -17,9 +18,20 @@ const Mypage = () => {
     try {
       const result = await updateProfile(formData);
       updateUserNickname(result.nickname);
-      alert("닉네임이 변경되었습니다");
+      Swal.fire({
+        title: "닉네임 변경 성공",
+        text: `${result.nickname}으로 닉네임을 변경하셨습니다.`,
+        icon: "success",
+        confirmButtonColor: "#a5b4fc",
+      });
     } catch (e) {
       console.error(e);
+      Swal.fire({
+        title: "닉네임 변경 오류",
+        text: e.response.data.message,
+        icon: "error",
+        confirmButtonColor: "#a5b4fc",
+      });
     }
   };
 
