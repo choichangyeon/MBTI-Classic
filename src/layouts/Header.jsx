88e7b955055e661b987bd572badbd96ec8058035
choichangyeon <changyon99@naver.com>
2025-02-25 10:32:18 +0900

    design:#18 헤더 버튼 스타일 수정

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -13,10 +13,18 @@ const Header = () => {
   return (
     <div className="w-full h-md row-base justify-between items-center px-10 bg-amber-100 shadow-md relative z-10">
       <div className="row-base">
-        <button className="mr-5" onClick={() => navigate("/")}>
+        <button
+          className="btn-base px-1 py-1 mr-5"
+          onClick={() => navigate("/")}
+        >
           홈
         </button>
-        <button onClick={() => navigate("/result")}>결과 페이지</button>
+        <button
+          className="btn-base px-1 py-1"
+          onClick={() => navigate("/result")}
+        >
+          결과 페이지
+        </button>
       </div>
       {isAuth ? <AuthButton /> : <MypageButton />}
     </div>
